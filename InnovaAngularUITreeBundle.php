02f23ec460e7d4ac135508ec2c8e83554486af4b
Elorfin <penin.axel@gmail.com>
2015-05-12 14:51:14 +0200

    upgrade + remove dependency to Claroline

--- InnovaAngularUITreeBundle.php
+++ InnovaAngularUITreeBundle.php
@@ -3,18 +3,8 @@
 namespace Innova\AngularUITreeBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Claroline\KernelBundle\Bundle\AutoConfigurableInterface;
-use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
 
-class InnovaAngularUITreeBundle extends Bundle implements AutoConfigurableInterface
+class InnovaAngularUITreeBundle extends Bundle
 {
-    public function supports($environment)
-    {
-        return true;
-    }
-    
-    public function getConfiguration($environment)
-    {
-        return new ConfigurationBuilder();
-    }
+
 }
